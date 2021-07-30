8ba5dc43009a5aa7caf11c731a814088783a98be
soulsam480 <soulsam480@hotmail.com>
2021-07-30 22:04:27 +0530

    feat: add express type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('working');
+require('tsconfig-paths/register');
+import { join } from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: join(__dirname, '../.env') });
+const PORT = process.env.PORT || 3000;
+import express from 'express';
+import cors from 'cors';
+
+async function main() {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get('/', (req, res) => res.send('Hello'));
+
+  app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+}
+
+main();
